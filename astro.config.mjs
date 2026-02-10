dd0eff2c40d95d1fc9fd6b62a4334846510973d9
Kurant <kurant@mini.local>
2026-02-10 20:39:02 +0100

    add sprite assets to public

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,9 @@ export default defineConfig({
   integrations: [react()],
 
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    server: {
+      allowedHosts: 'all'
+    }
   }
 });
